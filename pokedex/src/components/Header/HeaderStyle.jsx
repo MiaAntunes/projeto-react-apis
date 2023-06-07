d5f9499842d8410335baf51c23113e863da8cf62
MiaAntunes <suportecamilaantunes2003@hotmail.com>
2023-06-07 17:50:17 -0300

    Concluida a dinâmica dos botões

--- pokedex/src/components/Header/HeaderStyle.jsx
+++ pokedex/src/components/Header/HeaderStyle.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
+    display: flex;
     padding: 1em;
     width: 100vw;
     height: 10vh;
-    display: flex;
     align-items: center;
     justify-content: space-around;
     background:#fff;
@@ -16,18 +16,24 @@ export const ButtonListPokemon = styled.button`
     font-weight: 700;
     font-size: 1em;
     text-decoration-line: underline;
+    position: absolute; 
+    left: 5%;
 `
 
+// ! Colocar um position na LOGO
 export const Logo = styled.img`
-    width: calc(100% - 1250px);
+    width: 12em;
+    position:absolute;
+    left: 45%;
 `
 
 export const ButtonPokedex = styled.button`
-    background: #33A4F5;
     font-family: 'Poppins', sans-serif;
     font-weight: 700;
     font-size: 1em;
     heigth:2em;
     width:12em;
     color: #FFF;
+    position: absolute;
+    right:10%;
 `
